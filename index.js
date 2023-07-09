8741f84b544304dd1f169393629713bf8e213598
Daexaf <ihsan64.mi@gmail.com>
2023-07-09 12:30:59 +0700

    fixing midtrans

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const express = require("express"); // Import express library
 const app = express(); // Import express
 const cors = require("cors"); // Import cors
 const commonHelper = require("./src/helper/common");
-const midtransClient = require("midtrans-client");
 
-const midtransCore = new midtransClient.CoreApi({
-  isProduction: false,
-  serverKey: "Mid-server-qLSIv_yn0EW-zBYRAele6vqJ",
-  clientKey: "Mid-client-thHn1qpXvtWgFObg",
-});
 // Use middleware
 app.use(express.json());
 app.use(
@@ -45,30 +39,42 @@ app.listen(port, () => {
   console.log(`Server run on port: ${port}`);
 });
 
-app.post("/process-payment", async (req, res) => {
-  try {
-    // Menyiapkan payload pembayaran
-    const paymentPayload = {
-      transaction_details: {
-        order_id: "ORDER_ID",
-        gross_amount: 100000,
-      },
-      credit_card: {
-        secure: true,
-      },
-    };
-
-    // Melakukan pembayaran
-    const response = await midtransCore.charge(paymentPayload);
-
-    // Menyimpan informasi pembayaran ke database atau melakukan tindakan lainnya
-
-    // Mengirim respon ke frontend
-    res.json(response);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "Terjadi kesalahan dalam pemrosesan pembayaran" });
-  }
+const midtransClient = require("midtrans-client");
+// Create Snap API instance
+let snap = new midtransClient.Snap({
+  // Set to true if you want Production Environment (accept real transaction).
+  isProduction: false,
+  serverKey: "SB-Mid-server-o8AeFK89-NJwJwf6QY1RMTgu",
 });
+
+let parameter = {
+  transaction_details: {
+    order_id: "fb935959-7e79-4aa1-b531-7904731e4d1d",
+    gross_amount: 30000,
+  },
+  credit_card: {
+    secure: true,
+  },
+  customer_details: {
+    first_name: "Ihsan",
+    last_name: "dax",
+    email: "ihsan.pra@example.com",
+    phone: "08111222333",
+  },
+};
+
+snap.createTransaction(parameter).then((transaction) => {
+  // transaction token
+  let transactionToken = transaction.token;
+  console.log("transactionToken:", transactionToken);
+});
+
+snap
+  .createTransaction(parameter)
+  .then((res) => {})
+  .catch((e) => {
+    e.message; // basic error message string
+    e.httpStatusCode; // HTTP status code e.g: 400, 401, etc.
+    e.ApiResponse; // JSON of the API response
+    e.rawHttpClientData; // raw Axios response object
+  });
